7582a5ae7288f5a3ac063370da6fe788ecdb427e
himanshu1221 <himanshuchhatwal9295@gmail.com>
2025-05-16 07:51:11 +0530

    made a password verifier

---
+++ password_strength.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+password = sys.argv[1]
+
+def passwordvalidation():
+    if len(password) < 8:
+        print("Length of password is too short")
+    elif re.search(r"[A-Z]", password) is None:
+        print("Pass must contain upper and lower case letters")
+    elif re.search(r"[a-z]", password) is None:
+        print("Pass must contain upper and lower case letters")
+    elif re.search(r"\d", password) is None:
+        print("Pass must contain at least one digit")
+    elif re.search(r"[!@#$%^&*(),.?\":{}|<>]", password) is None:
+        print("Pass must contain at least one special character")
+    else:
+        print("Password is strong")
+
+# Call the function
+passwordvalidation()
